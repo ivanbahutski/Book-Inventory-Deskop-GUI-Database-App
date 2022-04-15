e1005f4804daca3acddccd6ef203794c84876b59
ivanbahutski <ivanbogutski1995@gmail.com>
2022-04-15 02:04:05 +0200

    Attached backend to frontend

--- backend.py
+++ backend.py
@@ -2,18 +2,18 @@ import sqlite3
 
 
 def connect():
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
     cur.execute(
         "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY,"
-        " title TEXT, author TEXT, yeaar INTEGER, isbn INTEGER )"
+        " title TEXT, author TEXT, year INTEGER, isbn INTEGER )"
     )
     conn.commit()
     conn.close()
 
 
 def insert(title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
     cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)", (title, author, year, isbn))
     conn.commit()
@@ -21,7 +21,7 @@ def insert(title, author, year, isbn):
 
 
 def view():
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM book")
     rows = cur.fetchall()
@@ -30,7 +30,7 @@ def view():
 
 
 def search(title="", author="", year="", isbn=""):
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
     rows = cur.fetchall()
@@ -39,7 +39,7 @@ def search(title="", author="", year="", isbn=""):
 
 
 def delete(id):
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
     cur.execute("DELETE FROM book WHERE id=?", (id,))
     conn.commit()
@@ -47,9 +47,9 @@ def delete(id):
 
 
 def update(id, title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
+    conn = sqlite3.connect("all_books.db")
     cur = conn.cursor()
-    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (id, title, author, year, isbn))
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
     conn.commit()
     conn.close()
 
